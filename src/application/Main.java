dd4de8bb8dffcd78af744eaa91d5b2e20e1e6f37
leandrochs <leandrochagask@gmail.com>
2025-09-02 20:15:10 -0300

    Initial commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,7 @@ package application;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello world!");
+        System.out.print("Hello world!");
 
     }
 }

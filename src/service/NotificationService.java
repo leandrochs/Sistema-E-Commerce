7d24c50f185b843f6a970d2e1abd57b80ec220aa
leandrochs <leandrochagask@gmail.com>
2025-09-04 13:02:24 -0300

    feat: adiciona simulacao de notificacao para o cliente informando que o pagamento foi confirmado

--- src/service/NotificationService.java
+++ src/service/NotificationService.java
@@ -15,7 +15,16 @@ public class NotificationService {
         System.out.println("--------------------------------------------------");
     }
 
-    public void sendPaymentConfirmedNotification(Customer customer, Order order) {}
+    public void sendPaymentConfirmedNotification(Customer customer, Order order) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("E-mail para: " + customer.getName() + " (ID: " + customer.getId() + ")");
+        System.out.println("Assunto: Pagamento do seu pedido #" + order.getId() + " confirmado!");
+        System.out.println("Prezado(a) " + customer.getName() + ",");
+        System.out.println("Temos o prazer de informar que o pagamento do seu pedido #" + order.getId() + " foi confirmado com sucesso.");
+        System.out.println("Em breve, seu pedido será preparado para entrega.");
+        System.out.println("Agradecemos a sua compra!");
+        System.out.println("--------------------------------------------------");
+    }
 
     public void sendDeliveryNotification(Customer customer, Order order) {}
 

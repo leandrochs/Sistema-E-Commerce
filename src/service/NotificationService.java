4d6b7d3043991f575890347f268f576e81340dd3
leandrochs <leandrochagask@gmail.com>
2025-09-04 11:38:37 -0300

    feat: adiciona classe NotificationService
    Dedicada a enviar e-mails para o cliente

---
+++ src/service/NotificationService.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.Customer;
+import model.Order;
+
+public class NotificationService {
+
+    public void sendPaymentPendingNotification(Customer customer, Order order) {}
+
+    public void sendPaymentConfirmedNotification(Customer customer, Order order) {}
+
+    public void sendDeliveryNotification(Customer customer, Order order) {}
+
+
+}

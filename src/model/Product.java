0818f44918f30363eb2c4a8916561d57ba32d409
leandrochs <leandrochagask@gmail.com>
2025-09-04 13:16:54 -0300

    feat: implementa registerProduct que cadastra um novo produto no sistema

--- src/model/Product.java
+++ src/model/Product.java
@@ -5,16 +5,24 @@ import java.util.UUID;
 
 public class Product {
 
-    private final UUID id;
+    private final String id;
     private final String name;
     private final String description;
     private final BigDecimal value;
 
-    public Product(UUID id, String name, String description, BigDecimal value) {
+    public Product(String id, String name, String description, BigDecimal value) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
     }
 }
 
--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -3,6 +3,7 @@ package service;
 import model.Product;
 import util.IdGenerator;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,12 +11,19 @@ public class ProductService {
     private final IProductRepository productRepository;
     private final IdGenerator idGenerator;
 
-    public ProductService(IProductRepository productRepository, IdGenerator idGenerator, IProductRepository productRepository1, IdGenerator idGenerator1) {
-        this.productRepository = productRepository1;
-        this.idGenerator = idGenerator1;
+    public ProductService(IProductRepository productRepository, IdGenerator idGenerator) {
+        this.productRepository = productRepository;
+        this.idGenerator = idGenerator;
     }
 
-    public void registerProduct(String name, String description, double value) {}
+    public void registerProduct(String name, String description, BigDecimal value) {
+        String id = idGenerator.generateId();
+        Product product = new Product(id, name, description, value);
+        productRepository.save(product);
+        System.out.println("--------------------------------------------------");
+        System.out.println("Produto '" + product.getName() + "' (ID: " + product.getId() + ") cadastrado com sucesso.");
+        System.out.println("--------------------------------------------------");
+    }
 
     public Optional<Product> findProductById(String id) {}
 

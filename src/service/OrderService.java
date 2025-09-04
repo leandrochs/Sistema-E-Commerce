99b583af45931ada9430a55683be8d7de90f28a3
leandrochs <leandrochagask@gmail.com>
2025-09-04 12:47:57 -0300

    feat: adiciona construtor

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -12,7 +12,13 @@ public class OrderService {
     private final NotificationService notificationService;
     private final IdGenerator idGenerator;
 
-    public OrderService(IOrderRepository orderRepository, ICustomerRepository customerRepository, IProductRepository productRepository, NotificationService notificationService, IdGenerator idGenerator) {}
+    public OrderService(IOrderRepository orderRepository, ICustomerRepository customerRepository, IProductRepository productRepository, NotificationService notificationService, IdGenerator idGenerator, IOrderRepository orderRepository1, ICustomerRepository customerRepository1, IProductRepository productRepository1, NotificationService notificationService1, IdGenerator idGenerator1) {
+        this.orderRepository = orderRepository1;
+        this.customerRepository = customerRepository1;
+        this.productRepository = productRepository1;
+        this.notificationService = notificationService1;
+        this.idGenerator = idGenerator1;
+    }
 
     public Order createOrder(String customerId) {}
 

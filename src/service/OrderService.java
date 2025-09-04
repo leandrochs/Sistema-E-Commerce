05391994dc22087c914c89e59b452cc9956d92ba
leandrochs <leandrochagask@gmail.com>
2025-09-04 11:35:15 -0300

    feat: adiciona classe OrderService
    Classe central para o fluxo de vendas, gerenciando a criacao, modificacao, finalizacao, pagamento e entrega de pedidos

---
+++ src/service/OrderService.java
@@ -0,0 +1,37 @@
+package service;
+
+import model.Order;
+import util.IdGenerator;
+
+import java.util.Optional;
+
+public class OrderService {
+    private final IOrderRepository orderRepository;
+    private final ICustomerRepository customerRepository;
+    private final IProductRepository productRepository;
+    private final NotificationService notificationService;
+    private final IdGenerator idGenerator;
+
+    public OrderService(IOrderRepository orderRepository, ICustomerRepository customerRepository, IProductRepository productRepository, NotificationService notificationService, IdGenerator idGenerator) {}
+
+    public Order createOrder(String customerId) {}
+
+    public void addItemToOrder(String orderId, String productId, int quantity, double saleValue) {}
+
+    public void removeItemFromOrder(String orderId, String productId) {}
+
+    public void updateItemQuantity(String orderId, String productId, int newQuantity) {}
+
+    public void finalizeOrder(String orderId) {}
+
+    public void processPayment(String orderId) {}
+
+    public void registerDelivery(String orderId) {}
+
+    public Optional<Order> findOrderById(String id) {}
+
+    public List<Order> listAllOrders() {}
+
+
+
+}
